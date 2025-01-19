0d2a5bb1cd5754378b3140b3aadb30d5527524d4
nlosou <2128819713@qq.com>
2025-01-19 22:03:44 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.the_road_to_android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.the_road_to_android"
