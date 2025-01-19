7a9dcf355bd17b322f47a5f292aa9cc30250c578
nlosou <2128819713@qq.com>
2025-01-19 21:57:57 +0800

    update

--- The_road_to_Android/app/src/main/java/com/example/the_road_to_android/MainActivity.kt
+++ The_road_to_Android/app/src/main/java/com/example/the_road_to_android/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     The_road_to_AndroidTheme {
-        Greeting("Android")
+        Greeting("Andr")
     }
 }
