c053364c3c0fc216bd30194ae7892cb82a5d9956
nlosou <2128819713@qq.com>
2025-01-20 17:51:37 +0800

    update

---
+++ app/src/main/java/com/example/the_road_to_android/Person.kt
@@ -0,0 +1,17 @@
+package com.example.the_road_to_android
+
+class Person {
+    var name=""
+    var age=0
+    fun  eat(){
+        println("dsjdjdjjd")
+    }
+}
+
+fun main()
+{
+    var  p=Person()
+    p.name="Noser"
+    p.age=21
+    p.eat()
+}
