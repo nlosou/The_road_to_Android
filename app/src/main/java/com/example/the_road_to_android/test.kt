7dd46de8b0d3ed58d496105dc014d73f7073331e
nlosou <2128819713@qq.com>
2025-01-20 17:24:12 +0800

    update

--- app/src/main/java/com/example/the_road_to_android/test.kt
+++ app/src/main/java/com/example/the_road_to_android/test.kt
@@ -3,7 +3,17 @@ package com.example.the_road_to_android
 fun main()
 {
     println(add(2,2))
+    checkNumber(100)
 }
 fun add(a:Int,b:Int):Int{
     return  a+b
+}
+
+fun checkNumber(number: Number){
+    when(number)
+    {
+        is Int-> println("number is int")
+        is Double->println("number is DOuble")
+        else-> println("number not support")
+    }
 }
