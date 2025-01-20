f65ddc5554e52df5d168b65a043f212d7d1a73cf
nlosou <2128819713@qq.com>
2025-01-20 16:09:48 +0800

    update

--- app/src/main/java/com/example/the_road_to_android/MainActivity.kt
+++ app/src/main/java/com/example/the_road_to_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.the_road_to_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -27,6 +28,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        Log.d("MainActivity","onCreate execute");
     }
 }
 
