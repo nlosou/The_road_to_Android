520181aecf4e7ded6b86800817f0430e61d18e65
nlosou <2128819713@qq.com>
2025-01-21 15:45:19 +0800

    update

--- app/src/main/java/com/example/the_road_to_android/ui/FirstActivity.kt
+++ app/src/main/java/com/example/the_road_to_android/ui/FirstActivity.kt
@@ -1,10 +1,9 @@
 package com.example.the_road_to_android.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import com.example.the_road_to_android.R
 
 class FirstActivity : AppCompatActivity() {
