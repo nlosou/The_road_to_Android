347b4adf2afbb7d252b80507cc4baa4a84933970
nlosou <2128819713@qq.com>
2025-01-21 16:34:28 +0800

    update

--- app/src/main/java/com/example/the_road_to_android/ui/FirstActivity.kt
+++ app/src/main/java/com/example/the_road_to_android/ui/FirstActivity.kt
@@ -1,14 +1,26 @@
 package com.example.the_road_to_android.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.the_road_to_android.R
-
+import com.example.the_road_to_android.databinding.FirstLayoutBinding
 class FirstActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: FirstLayoutBinding  // 声明绑定类
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_layout)
+        binding = FirstLayoutBinding.inflate(layoutInflater)  // 使用绑定类初始化布局
+        setContentView(binding.root)  // 设置内容视图
+
+        // 使用绑定类访问按钮
+        binding.buttonPanel.setOnClickListener {
+            Toast.makeText(this, "启动", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
