78c2cac1e4ddd8b43a5e9f0b262dec2fb6542592
nlosou <2128819713@qq.com>
2025-01-22 22:54:45 +0800

    update

---
+++ app/src/main/java/com/example/the_road_to_android/ui/ThirdActivity.kt
@@ -0,0 +1,17 @@
+package com.example.the_road_to_android.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.the_road_to_android.R
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third)
+
+    }
+}
